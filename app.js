d4062eb122a6b07a3286feb4271888186f5e9a66
Ahmad <benos.tarabain@gmail.com>
2020-10-06 01:09:56 +0300

    added observer

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ const options = {
   suffix: "+",
 };
 
-window.onload = function () {
+//add a check array to check if the user already saw the stats before, if he did, the counting animation wont
+//restart and the numbers stay as they are. If it's the first time he see the stats, then the counting animation is active
+var check = [];
+
+const observerOptions = {
+  threshold: 0.2,
+};
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    let div = entry.target;
+    console.log(div);
+    if (check.length === 0) {
+      startCount();
+    } else {
+      return;
+    }
+  });
+}, observerOptions);
+observer.observe(document.getElementById("number-section"));
+
+function startCount() {
+  check.push({ msg: "emitted" });
   const costumers = new CountUp("clients", 0, 14811, 0, 9, options);
   costumers.start();
 
@@ -16,4 +37,4 @@ window.onload = function () {
 
   const vps = new CountUp("vps", 0, 8046, 0, 7, options);
   vps.start();
-};
+}
